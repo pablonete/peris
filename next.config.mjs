8b0d5717437601ddba14da632278d01d63c85ffa
pablonete <pablonete@gmail.com>
2026-02-07 16:31:15 +0100

    Support localhost without extra folder

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
-  basePath: "/peris",
+  basePath: process.env.NODE_ENV === "production" ? "/peris" : "",
   typescript: {
     ignoreBuildErrors: true,
   },
